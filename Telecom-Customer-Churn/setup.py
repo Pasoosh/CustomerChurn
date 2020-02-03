60b8b09a516df85f86417e36e9adecf099f74b32
dos Passos <Leonardo.dosPassos@standardbank.co.za>
2020-02-03 18:54:48 +0200

    created folder structure using cookie cutter

---
+++ Telecom-Customer-Churn/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Use the data to try to predict customer churn',
+    author='Leonardo dos Passos',
+    license='',
+)
